803ccccc83eda9e613b2de946650e7bd34cb8a50
leeshien <ganleeshien@gmail.com>
2020-10-23 01:05:00 +0800

    Update shuttlecock.py

--- examples/FasterRCNN/dataset/shuttlecock.py
+++ examples/FasterRCNN/dataset/shuttlecock.py
@@ -74,7 +74,7 @@ class Shuttlecock(DatasetSplit):
 
                     annos = obj["shapes"]
 
-                    poly = np.asarray(annos[0]["points])
+                    poly = np.asarray(annos[0]["points"])
 #                     poly = np.asarray(list(map(int, annos["points"])))
                     maxxy = poly.max(axis=0)
                     minxy = poly.min(axis=0)
