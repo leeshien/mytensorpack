8d390136f4ac4a1de7b07d304c40f382b68c2bd0
leeshien <ganleeshien@gmail.com>
2020-10-22 21:57:07 +0800

    Update shuttlecock.py

--- examples/FasterRCNN/dataset/shuttlecock.py
+++ examples/FasterRCNN/dataset/shuttlecock.py
@@ -12,7 +12,7 @@ from utils.np_box_ops import area as np_area
 from utils.np_box_ops import iou as np_iou
 from common import polygons_to_mask
 
-__all__ = ["register_ic"]
+__all__ = ["register_shuttlecock"]
 
 
 class Shuttlecock(DatasetSplit):
@@ -37,42 +37,45 @@ class Shuttlecock(DatasetSplit):
         return x,y
 
     def training_roidbs(self):
-        files = [f for f in os.listdir(self.imgdir) if os.path.isfile(os.path.join(self.imgdir, f))]
-        jsonfiles = [f for f in files if f.endswith('.json')]
-        imgfiles = [f for f in files if f.endswith('.jpeg') or f.endswith('.jpg')]
-
         ret = []
         boxes = []
-        segs = []        
-        for fn in jsonfiles:
-            json_file = os.path.join(self.imgdir, fn)
-            with open(json_file) as f:
-                obj = json.load(f)
+        segs = []         
+        for (path, b, files) in walk(self.imgdir):
 
-            try:
-                fname = [filename for filename in imgfiles if fn.split('.')[0] in filename][0] #image filename
-                fname = os.path.join(self.imgdir, fname)
+            # files = [f for f in os.listdir(self.imgdir) if os.path.isfile(os.path.join(self.imgdir, f))]
+            jsonfiles = [f for f in files if f.endswith('.json')]
+            imgfiles = [f for f in files if f.endswith('.jpeg') or f.endswith('.jpg')]
 
-                roidb = {"file_name": fname}
+       
+            for fn in jsonfiles:
+                json_file = os.path.join(self.imgdir, fn)
+                with open(json_file) as f:
+                    obj = json.load(f)
 
-                annos = obj["shapes"]
+                try:
+                    fname = [filename for filename in imgfiles if fn.split('.')[0] in filename][0] #image filename
+                    fname = os.path.join(self.imgdir, fname)
 
-                poly = np.asarray(poly)
-                maxxy = poly.max(axis=0)
-                minxy = poly.min(axis=0)
-                
-                boxes.append([minxy[0], minxy[1], maxxy[0], maxxy[1]])            
+                    roidb = {"file_name": fname}
+
+                    annos = obj["shapes"]
+
+                    poly = np.asarray(poly)
+                    maxxy = poly.max(axis=0)
+                    minxy = poly.min(axis=0)
                     
-                N = 1
-                roidb["boxes"] = np.asarray(boxes, dtype=np.float32)
-                roidb["segmentation"] = [[poly]]
+                    boxes.append([minxy[0], minxy[1], maxxy[0], maxxy[1]])            
+                        
+                    N = 1
+                    roidb["boxes"] = np.asarray(boxes, dtype=np.float32)
+                    roidb["segmentation"] = [[poly]]
 
-                roidb["class"] = np.ones((N, ), dtype=np.int32)
-                roidb["is_crowd"] = np.zeros((N, ), dtype=np.int8)
-                ret.append(roidb) 
-            except Exception as e:
-                print(fn, ' not does matched with any image')
-                pass  
+                    roidb["class"] = np.ones((N, ), dtype=np.int32)
+                    roidb["is_crowd"] = np.zeros((N, ), dtype=np.int8)
+                    ret.append(roidb) 
+                except Exception as e:
+                    print(fn, ' not does matched with any image')
+                    pass  
 
             return ret
 
